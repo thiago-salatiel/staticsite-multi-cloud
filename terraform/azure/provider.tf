ecf6ed09d5408c707859c5e80636d18fe85fd407
Kledson Basso <kledsonhugo@gmail.com>
2023-10-20 16:04:05 -0300

    Azure backend config

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -10,6 +10,10 @@ terraform {
     storage_account_name = "straccttf001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    client_id            = "cb546c13-bc90-4ba2-80ff-27e97f14cf7d"
+    client_secret        = "BgZ8Q~rIXSmwh.oX3VSmEsV3EvkNLO_xjAqHSdrH"
+    tenant_id            = "11dbbfe2-89b8-4549-be10-cec364e59551"
+    subscription_id      = "fe880dd5-3ebe-4c1b-bb39-7a80be9d40bc"
   }
 }
 
@@ -17,8 +21,4 @@ provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "cloud"
-  client_id                  = "cb546c13-bc90-4ba2-80ff-27e97f14cf7d"
-  client_secret              = "BgZ8Q~rIXSmwh.oX3VSmEsV3EvkNLO_xjAqHSdrH"
-  tenant_id                  = "11dbbfe2-89b8-4549-be10-cec364e59551"
-  subscription_id            = "fe880dd5-3ebe-4c1b-bb39-7a80be9d40bc"
 }
