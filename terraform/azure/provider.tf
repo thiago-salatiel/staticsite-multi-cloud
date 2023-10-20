199f3b8d1edee951714e2315f0936824eebdfa61
Kledson Basso <kledsonhugo@gmail.com>
2023-10-20 15:22:35 -0300

    Azure backend config

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -2,8 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.76.0"
+      version = ">= 3.77.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform"
+    storage_account_name = "straccttf001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
